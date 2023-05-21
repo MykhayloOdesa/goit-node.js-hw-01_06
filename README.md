c817371e074a2ef828a1a6a1b790587ba64d257a
MykhayloOdesa <m.yervachov@gmail.com>
2023-05-21 18:25:12 +0300

    HW 1 completed

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # goit-node.js-hw-01_06
+
+# HW #1:
+
+# yargs /
+
+1. added package - https://prnt.sc/QnyKQ2L2gSXp
+2. action list - https://prnt.sc/5Rw7vaP_rE7A
+3. action get - https://prnt.sc/7sklDON0o3bw
+4. action add - https://prnt.sc/dvDbH5l7p5yu
+5. action remove - https://prnt.sc/GR5X8Gv2n90m
+
+# commander /
+
+1. added package - https://prnt.sc/gJLN1TVlUdIX
+2. action list - https://prnt.sc/EydQZWqvxuaB
+3. action get - https://prnt.sc/hzfBUpb9Z718
+4. action add - https://prnt.sc/O1ppDpNh4nuN
+5. action remove - https://prnt.sc/jqTvf2jyjUkt
--- index.js
+++ index.js
@@ -1,3 +1,81 @@
-const { listContacts } = require("./contacts");
+// ***FIRST WAY - USING YARGS***
 
-console.log(listContacts());
+// const contactService = require("./contacts");
+
+// const argv = require("yargs").argv;
+
+// async function invokeAction({ action, id, name, email, phone }) {
+// 	switch (action) {
+// 		case "list":
+// 			const allContacts = await contactService.listContacts();
+// 			console.log(allContacts);
+// 			break;
+
+// 		case "get":
+// 			const seekingContact = await contactService.getContactById(id);
+// 			console.log(seekingContact);
+// 			break;
+
+// 		case "add":
+// 			const newContact = await contactService.addContact(name, email, phone);
+// 			console.log(newContact);
+// 			break;
+
+// 		case "remove":
+// 			const deletedContact = await contactService.removeContact(id);
+// 			console.log(deletedContact);
+// 			break;
+
+// 		default:
+// 			console.warn("\x1B[31m Unknown action type!");
+// 	}
+// }
+
+// invokeAction(argv);
+
+// ***ALTERNATIVE WAY - USING COMMANDER***
+
+const contactService = require("./contacts");
+
+const { Command } = require("commander");
+const program = new Command();
+
+program
+	.option("-a, --action <type>", "choose action")
+	.option("-i, --id <type>", "user id")
+	.option("-n, --name <type>", "user name")
+	.option("-e, --email <type>", "user email")
+	.option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+	switch (action) {
+		case "list":
+			const allContacts = await contactService.listContacts();
+			console.log(allContacts);
+			break;
+
+		case "get":
+			const seekingContact = await contactService.getContactById(id);
+			console.log(seekingContact);
+			break;
+
+		case "add":
+			const newContact = await contactService.addContact(name, email, phone);
+			console.log(newContact);
+			break;
+
+		case "remove":
+			const deletedContact = await contactService.removeContact(id);
+			console.log(deletedContact);
+			break;
+
+		default:
+			console.warn("\x1B[31m Unknown action type!");
+	}
+}
+
+invokeAction(argv);
