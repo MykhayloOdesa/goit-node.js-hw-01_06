dac130c8ad9dd91749e307db67239e2c77d16f41
MykhayloOdesa <m.yervachov@gmail.com>
2023-07-03 18:16:11 +0300

    Update authRouter.js

--- routes/api/authRouter.js
+++ routes/api/authRouter.js
@@ -29,7 +29,7 @@ router.post('/logout', authenticate, logout);
 
 router.get('/current', authenticate, getCurrent);
 
-router.patch('/avatars', authenticate, upload.single('avatar'), updateAvatar);
+router.patch('/avatars', authenticate, upload.single('avatarURL'), updateAvatar);
 
 router.patch('/', authenticate, validateBody(updateSubscriptionSchema), updateSubscription);
 
