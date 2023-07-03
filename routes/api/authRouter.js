26c5e489398aedc2ebdc5f7752e34c713f468b41
MykhayloOdesa <m.yervachov@gmail.com>
2023-07-03 12:21:19 +0300

    added multer

--- routes/api/authRouter.js
+++ routes/api/authRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const { authenticate } = require('../../middlewares/authenticate');
+const { upload } = require('../../middlewares/upload');
 const { validateBody } = require('../../utils/validateBody');
 
 const {
@@ -26,6 +27,8 @@ router.post('/login', validateBody(loginSchema), login);
 router.post('/logout', authenticate, logout);
 
 router.get('/current', authenticate, getCurrent);
+
+router.patch('/avatars', authenticate, upload.single('avatarURL'), updateAvatar);
 
 router.patch('/', authenticate, validateBody(updateSubscriptionSchema), updateSubscription);
 
