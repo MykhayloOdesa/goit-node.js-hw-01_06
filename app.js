4a56f72abfac5a602ea9ca7b7234b97639af2512
MykhayloOdesa <m.yervachov@gmail.com>
2023-07-04 14:10:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
 
-const { authRouter } = require("./routes/api/authRouter");
-const { contactsRouter } = require("./routes/api/contactsRouter");
+const { authRouter } = require('./routes/api/authRouter');
+const { contactsRouter } = require('./routes/api/contactsRouter');
 
 const app = express();
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.use("/users", authRouter);
+app.use('/users', authRouter);
 
-app.use("/api/contacts", contactsRouter);
+app.use('/api/contacts', contactsRouter);
 
 app.use((_, res) => {
-  res.status(404).json({ message: "Not found" });
+  res.status(404).json({ message: 'Not found' });
 });
 
 app.use((error, _, res) => {
-  const { status = 500, message = "Internal Server Error" } = error;
+  const { status = 500, message = 'Internal Server Error' } = error;
   res.status(status).json({ message });
 });
 
